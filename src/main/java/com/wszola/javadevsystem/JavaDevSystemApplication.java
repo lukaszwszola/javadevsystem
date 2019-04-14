ac31f19af7f9313bf2cb9026530c75234579d5a6
Lukasz <lukasz.wszola@gmail.com>
2019-04-14 14:49:27 +0200

    Start project

---
+++ src/main/java/com/wszola/javadevsystem/JavaDevSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wszola.javadevsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevSystemApplication.class, args);
+	}
+
+}
